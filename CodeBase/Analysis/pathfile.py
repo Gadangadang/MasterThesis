b1fff818c09e669d690197b8e5963af8301530a9
Gadangadang <sakarias.frette@outlook.com>
2022-10-05 19:26:21 +0200

    moved three classes to utilities and created a script only for paths

---
+++ CodeBase/Analysis/pathfile.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+from os.path import isfile, join
+
+STORE_IMG_PATH = "/home/sgfrette/MasterThesis/Figures/testing/"
+DATA_PATH = Path("/storage/William_Sakarias/Sakarias_Data")
