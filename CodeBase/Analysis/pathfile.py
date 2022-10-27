f0e2d85a144349ac22c5e5a38ed8aa0d2dd5c832
Gadangadang <sakarias.frette@outlook.com>
2022-10-27 11:27:01 +0200

    Added model plots for the trained models with out a given channel

--- CodeBase/Analysis/pathfile.py
+++ CodeBase/Analysis/pathfile.py
@@ -3,3 +3,5 @@ from os.path import isfile, join
 
 STORE_IMG_PATH = "/home/sgfrette/MasterThesis/Figures/testing/"
 DATA_PATH = Path("/storage/William_Sakarias/Sakarias_Data")
+
+TF_MODEL_PATH = Path("/Users/Sakarias/MasterThesis/CodeBase/Analysis/tf_models")
---
+++ CodeBase/Analysis/visualize_models.py
@@ -0,0 +1,20 @@
+import tensorflow as tf 
+from pathfile import *
+from os.path import isfile, join
+from os import listdir
+
+
+trained_models = [f for f in listdir(TF_MODEL_PATH) if isfile(join(TF_MODEL_PATH, f))] 
+
+for model in trained_models:
+    
+    start = model.find("o_")
+    end = model.find(".h5")
+    
+    name = model[start+2:end]
+    print(name)
+    AE_model = tf.keras.models.load_model(
+                        "tf_models/" + model
+                    )
+
+    tf.keras.utils.plot_model(AE_model, to_file="/Users/Sakarias/MasterThesis/Figures/testing/model_plots/" + f"{name}_model_plot.pdf", show_shapes=True, show_layer_names=True, expand_nested=True)
