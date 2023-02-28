made correct change to the pt scaling test

CodeBase/Analysis/LEP2_testing.py
@@ -721,7 +721,7 @@ class LEP2ScaleAndPrep:
 
         #* Signal inference 
         sigs = np.unique(self.signal_categories)
-        sig = sigs[0]
+        sig = sigs[1]
         signame = sig[21:-9]
         sig_idx = np.where(self.signal_categories==sig)
         signal_cats = self.signal_categories.to_numpy()[sig_idx]
@@ -832,5 +832,5 @@ if __name__ == "__main__":
     
     #L2.mergeMegaBatches()
     
-    L2.RunTraining()
+    #L2.RunTraining()
     L2.RunInference()

CodeBase/Analysis/ScaleAndPrep.py
@@ -129,10 +129,13 @@ class ScaleAndPrep:
 
             df = pd.read_hdf(self.path / file)
             
+            
 
             name = file[: file.find("_3lep")]
             print(df.columns, len(df.columns))
-        
+
+            np.save(DATA_PATH / "dfcols.npy", np.asarray(df.columns))
+           
 
             if name in data_names:
                 name = "data"

CodeBase/Analysis/VAE.py
@@ -389,24 +389,25 @@ class RunVAE:
             n_bins = 25
 
         colors = [
-            "mediumspringgreen",
-            "darkgreen",
-            "lime",
-            "magenta",
-            "blue",
-            "red",
-            "orange",
-            "brown",
-            "cyan",
-            "mediumorchid",
-            "gold",
-            "darkgoldenrod",
-            
-        ]
+                "darkblue",
+                "gray",
+                "indigo",
+                "darkgoldenrod",
+                "cornflowerblue",
+                "royalblue",
+                "orangered",
+                "mediumspringgreen",
+                "darkgreen",
+                "blue",
+                "brown",
+                "mediumorchid",
+                "gold",
+               
+                
+            ]
         
         if len(colors) != len(histo_atlas):
-            colors = np.random.choice(colors, size=len(histo_atlas), replace=False)  
-        
+            colors = colors[:len(histo_atlas)]
         if len(histo_atlas) < 2:
             channels = ["Monte Carlo"]
             

CodeBase/Analysis/histo.py
@@ -141,24 +141,26 @@ class PlotHistogram:
             ]
         else:   
             colors = [
+                "darkblue",
+                "gray",
+                "indigo",
+                "darkgoldenrod",
+                "cornflowerblue",
+                "royalblue",
+                "orangered",
                 "mediumspringgreen",
                 "darkgreen",
-                "lime",
-                "magenta",
                 "blue",
-                "red",
-                "orange",
                 "brown",
-                "cyan",
                 "mediumorchid",
                 "gold",
-                "darkgoldenrod",
+               
                 
             ]
         
-        """if len(colors) != len(histo_atlas):
-            colors = np.random.choice(colors, size=len(histo_atlas), replace=False)  
-        """
+        if len(colors) != len(histo_atlas):
+            colors = colors[:len(histo_atlas)]
+        
         if len(histo_atlas) < 2:
             channels = ["Monte Carlo"]
             

CodeBase/Analysis/ptaltering.py
@@ -78,9 +78,43 @@ class pTAltering(model):
             signal = X_val[np.where(val_cat == "Signal")]
             X_val = X_val[np.where(val_cat != "Signal")]
             
-            signal[:, 0] = signal[:, 0]*ptscaling
+            cols = np.load(DATA_PATH / "dfcols.npy", allow_pickle=True)
             
-            print(np.shape(signal))
+            pt_ele0 = np.where(cols == "e_T_ele_0")
+            pt_muo0 = np.where(cols == "e_T_muo_0")
+            
+            df = pd.DataFrame(signal, columns = cols)
+            indexes1 = df.index[df["e_T_ele_0"] > 0.].to_list()
+            indexes2 = df.index[df["e_T_muo_0"] > 0.].to_list()
+            
+          
+            
+            
+            delta_et_ele1ele2 = np.where(cols == "delta_e_t_ele_1")
+            
+            e1 = signal[indexes1, pt_ele0] * (signal[indexes1, delta_et_ele1ele2]-1)/(-(signal[indexes1, delta_et_ele1ele2]+1))
+            signal[indexes1, pt_ele0] = signal[indexes1, pt_ele0]*ptscaling
+            
+            e0 = signal[indexes1, pt_ele0]
+            
+            signal[indexes1, delta_et_ele1ele2] = (e0-e1)/(e0+e1)
+            
+           
+            
+           
+            
+            delta_et_muo1 = np.where(cols == "delta_e_t_muo_1")
+            
+            e1 = signal[indexes2, pt_muo0] * (signal[indexes2, delta_et_muo1]-1)/(-(signal[indexes2, delta_et_muo1]+1))
+            signal[indexes2, pt_muo0] = signal[indexes2, pt_muo0]*ptscaling
+
+            e0 = signal[indexes2, pt_muo0]
+            
+            signal[indexes2, delta_et_muo1] = (e0-e1)/(e0+e1)
+            
+            print(np.shape(signal[indexes2, pt_muo0]))
+            
+            
             
             
             sample_weight_t = self.data_structure.weights_train.to_numpy().copy()
@@ -109,7 +143,7 @@ class pTAltering(model):
             et = time.time()
                 
             try:
-                img_path = Path(f"histo/{TYPE}/{arc}/{SCALER}/b_data_recon_big_rm3_feats_sig_fakedata.pdf")
+                img_path = Path(f"histo/{TYPE}/{arc}/{SCALER}/b_data_recon_big_rm3_feats_sig_pT_{ptscaling}.pdf")
                 path = STORE_IMG_PATH/img_path
 
                 files = {"photo":open(path, "rb")}